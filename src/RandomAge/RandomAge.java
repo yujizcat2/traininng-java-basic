d9eeb62043d8eede923c2f39f40721f6da46181d
yujizcat2 <yujizcat@gmail.com>
2025-04-02 13:28:03 +0900

    random age

---
+++ src/RandomAge/RandomAge.java
@@ -0,0 +1,10 @@
+package RandomAge;
+
+public class RandomAge {
+
+	public static void main(String[] args) {
+		int r = new java.util.Random().nextInt(100);
+		System.out.println("Is your age "+ r + " years old?");
+	}
+
+}
